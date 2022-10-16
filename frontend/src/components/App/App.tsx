b0d5ca65d9285419eb8fea64c1989cd21c27be38
tolmany <jollydev0821@gmail.com>
2022-10-16 03:27:26 -0400

    create project

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Nav } from "../Nav";
+import * as GuestBook from "../../contracts/guest-book";
+
+export function App() {
+  React.useEffect(() => {
+    // this is showing an overly-simple way to use the GuestBook import by just
+    // logging the result of the call to `getMessages`
+    GuestBook.getMessages().then(console.log);
+  }, []);
+
+  return (
+    <>
+      <Nav />
+      <main className="container">
+        <h1>Welcome</h1>
+        <p>Edit files and save to reload. Some interesting files:</p>
+        <ul>
+          <li>
+            <code>.env</code>
+          </li>
+          <li>
+            <code>src/utils/near.ts</code>
+          </li>
+          <li>
+            <code>src/components/Nav/Nav.tsx</code>
+          </li>
+          <li>
+            <code>src/contracts/guest-book.ts</code>
+          </li>
+          <li>
+            <code>src/components/App/App.tsx</code>
+          </li>
+        </ul>
+        <p>
+          Note that this site does not currently do anything with the contract
+          other than sign someone in. For an example of using the{" "}
+          <code>guest-book.testnet</code> contract, see{" "}
+          <a
+            href="https://github.com/near-examples/guest-book"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            near-examples/guest-book
+          </a>
+          . You may also want to learn more about{" "}
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          ,{" "}
+          <a
+            href="https://create-react-app.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            create-react-app
+          </a>
+          , and{" "}
+          <a
+            href="https://www.near.university"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            NEAR
+          </a>
+          .
+        </p>
+      </main>
+    </>
+  );
+}
